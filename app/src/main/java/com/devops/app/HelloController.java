92872795443ad8f045abad7360ac348ae5bee4d4
TomerHanan <tomerhanan9@gmail.com>
2026-02-26 19:16:10 +0200

    fixed controller errors

--- app/src/main/java/com/devops/app/HelloController.java
+++ app/src/main/java/com/devops/app/HelloController.java
@@ -1,12 +1,14 @@
-      Javapackage com.devops.app;
 
-      import org.springframework.web.bind.annotation.GetMapping;
-      import org.springframework.web.bind.annotation.RestController;
+      package com.devops.app;
 
-      @RestControllerpublic
-      class HelloController { 
-        @GetMapping("/")
-          public String index() { 
-                return "200 OK - Application is running!"; 
-                }
-        }
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "200 OK - Application is running!";
+    }
+}
